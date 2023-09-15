3500246504f61b1e0af37d3f4618fc7603ea2d08
Sumit Bose <doomonyou1712@gmail.com>
2023-09-15 16:36:56 +0530

    Hedaer Responsive

--- webtask/src/components/Header.jsx
+++ webtask/src/components/Header.jsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
+import './Header.css';
 
 const Header = () => {
+
+    const [menuActive, setMenuActive] = useState(false);
+
+    function handleMenu(){
+        setMenuActive(!menuActive);
+        if(menuActive === true){
+            document.querySelector('.navlinks').classList.add("active");
+        }
+        else if(menuActive === false){
+            document.querySelector('.navlinks').classList.remove("active");
+        }
+    }
+
   return (
     <div className="bg-white-500 p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="left-nav">
-          <ul className="flex space-x-4 pl-10 font-semibold sm:pl-4">
-            <li className="text-black sm:hidden">Menu</li>
-            <li className="text-black hidden sm:block">Home</li>
-            <li className="text-black hidden sm:block">About</li>
-            <li className="text-black hidden sm:block">Schedules</li>
-            <li className="text-black hidden sm:block">Membership</li>
-            <li className="text-black hidden sm:block">Pricing</li>
+        <div className="left-nav flex">
+            <ul className='Menu' onClick={handleMenu}>
+                <li className="text-black font-semibold ">Menu</li>
+            </ul>
+          <ul className="navlinks flex font-semibold">
+            <li className="text-black px-2">Home</li>
+            <li className="text-black px-2">About</li>
+            <li className="text-black px-2">Schedules</li>
+            <li className="text-black px-2">Membership</li>
+            <li className="text-black px-2">Pricing</li>
           </ul>
         </div>
         <div className="right-nav">
